283119cd60b7863ca87c1169eeb81f221539e133
Leonardo Gatti <leonardogattidev@gmail.com>
2023-12-25 23:32:40 -0300

    fix: 1-bruteforce missing error return type

--- src/solutions/1-bruteforce.zig
+++ src/solutions/1-bruteforce.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn twoSum(nums: []const isize, target: isize) [2]isize {
+pub fn twoSum(nums: []const isize, target: isize) ![2]isize {
     for (nums, 0..) |x, i| {
         const expected = target - x;
         for (nums, 0..) |y, j| {
